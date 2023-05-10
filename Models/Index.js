fc56f8ad55346f4d93f5e92c0c0b9699f0e6d7cf
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-05-10 11:09:00 -0500

    Added code into modules but have small error with userroutes

--- Models/Index.js
+++ Models/Index.js
@@ -1,8 +1,35 @@
-const router = require('express').router(); 
-const sequelize = require ('../config/connection.js');
-const {
-    Post, 
-    User, 
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('Models\Comment.js');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
     Comment
-} = require ('../models'); 
-const withAuth= require('../utils/auth');
+};
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
+const userRoutes = require('controllers\api\user-routes.js');
+const postRoutes = require('controllers\api\post-routes.js');
 const commentRoutes = require('controllers\api\comment-routes.js');
 
 router.use('/users', userRoutes);
