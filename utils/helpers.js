c9d116de61e09ffcbed4b35cd29fbafad59344dd
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-05-13 16:30:02 -0500

    Commnts added in utils, views, layout

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,17 @@
 module.exports = {
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    },
-    format_plural: (word, amount) => {
-        if (amount !== 1) {
-          return `${word}s`;
-        }
-    
-        return word;
+  // format a date string to MM/DD/YYYY format
+  formatDate: date => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+  },
+
+  // format a word to its plural form based on the given amount
+  formatPlural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
     }
+
+    return word;
   }
+};
