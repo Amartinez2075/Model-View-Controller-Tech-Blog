6371365003cda31becb60b4a3a16879e51fe248b
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-05-15 22:04:27 -0500

    Connection js update, layout 2 layouts

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
-// Packages needed for connection
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-if (process.env.JAWSDB_URL) {
-  // If JAWSDB_URL environment variable is present (indicating a remote database connection)
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // If JAWSDB_URL environment variable is not present (indicating a local database connection)
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+module.exports = sequelize;
