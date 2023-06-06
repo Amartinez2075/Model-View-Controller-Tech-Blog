8af90f9a0d037c8d8bc75588fbc30506ce9887d0
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-06-06 16:47:59 -0500

    Changing stuff

--- .js
+++ .js
@@ -2,12 +2,12 @@
 // If there isn't one, the user is redirected to the login page
 // Otherwise, the next middleware is called
 const withAuth = (req, res, next) => {
-  if (!req.session.user_id) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
-
-// The withAuth middleware function is exported so it can be used in other files
-module.exports = withAuth;
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  // The withAuth middleware function is exported so it can be used in other files
+  module.exports = withAuth;
